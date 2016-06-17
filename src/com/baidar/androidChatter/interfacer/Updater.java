20880afcc441f052a2444469e53a9759f16ba7cd
hoangbv58 <hoangbv_58@vnu.edu.vn>
2016-06-17 11:01:29 +0700

    First commit

--- src/com/baidar/androidChatter/interfacer/Updater.java
+++
@@ -1,9 +0,0 @@
-package com.baidar.androidChatter.interfacer;
-import com.baidar.androidChatter.typo.InfoOfFriend;
-import com.baidar.androidChatter.typo.InfoOfMessage;
-
-
-public interface Updater {
-	public void updateData(InfoOfMessage[] messages, InfoOfFriend[] friends, InfoOfFriend[] unApprovedFriends, String userKey);
-
-}
--- src/com/baidar/androidChatter/typo/InfoStatus.java
+++
@@ -1,5 +0,0 @@
-package com.baidar.androidChatter.typo;
-
-public enum InfoStatus {
-	ONLINE, OFFLINE, BUSY, INVISIBLE, AWAY, UNAPPROVED
-}
